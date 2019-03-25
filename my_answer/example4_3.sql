0cbcd49016108a709168bf188498322fb663c4ca
Satoshi Kataoka <k.3104is@gmail.com>
2019-03-25 23:10:11 +0900

    finished up to chapter 4

---
+++ my_answer/example4_3.sql
@@ -0,0 +1,14 @@
+CREATE TABLE ShohinSaeki
+(shohin_id    CHAR(4)       NOT NULL,
+ shohin_mei   VARCHAR(100)  NOT NULL,
+ hanbai_tanka INTEGER,
+ shiire_tanka INTEGER,
+ saeki        INTEGER,
+ PRIMARY KEY(shohin_id));
+
+
+
+ INSERT INTO ShohinSaeki
+ SELECT shohin_id, shohin_mei, hanbai_tanka, shiire_tanka,
+    hanbai_tanka - shiire_tanka AS saeki
+FROM Shohin;
---
+++ my_answer/example4_4.sql
@@ -0,0 +1,4 @@
+UPDATE ShohinSaeki
+SET hanbai_tanka = 3000,
+ saeki = hanbai_tanka - shiire_tanka
+ WHERE shohin_mei = 'カッターシャツ';
